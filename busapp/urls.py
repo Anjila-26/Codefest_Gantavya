ce608748327cd19f3f349f6b8a8fbe232bdb5685
Anjila subedi <anjilakshetri@gmail.com>
2023-10-09 14:10:00 +0545

    added leaflet map with the website

--- busapp/urls.py
+++ busapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-
+    path('project',click_photo_enter, name='project' ),
 ]
--- busapp/views.py
+++ busapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def click_photo_enter(request):
+
+
+    return render(request, 'index.html')
